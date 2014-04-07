0ada1faf9e1b126b3ece882d65b7c42de2e564aa
Oliver Hellebusch <oliver.hellebusch@sinnerschrader.com>
2014-04-07 15:54:05 +0200

    add grunt-contrib-nodeunit dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,19 +5,33 @@ module.exports = function (grunt) {
     jshint: {
       all: [
         'Gruntfile.js',
-        'lib/*.js'
+        'lib/*.js',
+        '<%= nodeunit.test %>'
       ],
       options: {
         jshintrc: '.jshintrc'
       }
+    },
+
+    nodeunit: {
+      options: {
+        reporter: 'verbose'
+      },
+      test: ['test/*_test.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  grunt.registerTask('travis', ['jshint', 'nodeunit']);
 
   grunt.registerTask('default', 'Info screen', function () {
     grunt.log.subhead('### grunt tasks:\n');
-    grunt.log.writeln('  grunt jshint');
+    grunt.log.writeln('  grunt jshint         js linting');
+    grunt.log.writeln('  grunt nodeunit       run test suite');
+    grunt.log.writeln('');
+    grunt.log.writeln('  grunt travis         runs jshint and nodeunit tasks');
     grunt.log.writeln('');
   });
 };
